bfee037ccbe382efd00e133b862a4e0520f79a92
Khubaib <mohmmedkhubaib2001@gmail.com>
2023-12-16 12:31:46 +0530

    Programs Modified

--- 13_a.js
+++ 13_a.js
@@ -1,7 +1,7 @@
 var http = require('http')
-var dt = require('./Module')
+var dt = require('./13_b')
 http.createServer(function(req,res){
     res.writeHead(200,{'ContentType':'Text/html'})
     res.write("The Current Time and Date : "+dt.myDateTime())
-    res.end
+    res.end();
 }).listen(8080)
--- 14.js
+++ 14.js
@@ -1,7 +1,7 @@
 var http = require('http')
 var fs = require('fs')
 http.createServer(function (req , res){
-fs.readFile('index.html',function(err, data){
+fs.readFile('1.html',function(err, data){
     res.writeHead(200 ,{ 'ContentType':'text/html'});
     res.write(data);
     return res.end();
